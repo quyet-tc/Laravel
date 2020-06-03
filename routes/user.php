acebee9c0b09c7c5e1bc37843153e14594b2b6d3
quyet-tc <53265406+quyet-tc@users.noreply.github.com>
2020-06-03 08:31:02 +0700

    up code

--- routes/user.php
+++ routes/user.php
@@ -1,6 +1,6 @@
 <?php
-Route::get('/', "HomeController@index");
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/',"WebController@index");
+Route::get('/home','HomeController@index')->name('home');
 
 Route::get("/demo-routing","WebController@demoRouting");
 Route::get("/category/{category:slug}","HomeControllerA@category");
